d518fc5994c4dbd8f1b9eacbfcac3a3024ebe455
Thursdaymood <thedeliveredream@gmail.com>
2023-03-04 18:31:34 +0700

    data structure java

--- java/DataStructure/src/DSOne.java
+++ java/DataStructure/src/DSOne.java
@@ -1,9 +1,42 @@
 
+import java.util.Stack;
+
 public class DSOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		referenceStudy();
 	}
+	
+	public void one_Stack() {
+		
+		/* Stack /LIFO - Last-in First-out/
+		 * push()	add item
+		 * pop()	remove
+		 * peak()	know the previous pushed item
+		 * empty()	return the boolean check item in stack
+		 * search(x)	return 1 means the stack has x, -1 means opposite
+		 */
+		Stack<String> stackTest = new Stack<String>();
+		
+		System.out.println("Check stack's capacity: "+stackTest.empty());
+		stackTest.push("Minecraft");
+		
+	}
+	
+	public static void referenceStudy() {
+		 
+		 theLine(30);
+		 System.out.println("    "+"Lecture from Bro Code");
+		 System.out.println("    URL: \"https://www.youtube.com/watch?v=CBYHwZcbD-s\"");
+		 theLine(30);
+	}
+	public static void theLine(int num) {
+		
+		for(int i = 0 ; i < num ; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
+	}
+	
 
 }
