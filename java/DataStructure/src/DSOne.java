a91040936323f898d3fc7625ee30d41bed384d45
Thursdaymood <thedeliveredream@gmail.com>
2023-03-04 18:54:09 +0700

    Update DSOne.java

--- java/DataStructure/src/DSOne.java
+++ java/DataStructure/src/DSOne.java
@@ -2,24 +2,46 @@
 import java.util.Stack;
 
 public class DSOne {
-
+	
 	public static void main(String[] args) {
+		String[] details = {"Stack", "Queue"};
 		referenceStudy();
+		detailsInDSOne(details);
 	}
 	
-	public void one_Stack() {
+	public static void one_Stack() {
 		
 		/* Stack /LIFO - Last-in First-out/
 		 * push()	add item
 		 * pop()	remove
-		 * peak()	know the previous pushed item
+		 * peek()	know the previous pushed item
 		 * empty()	return the boolean check item in stack
 		 * search(x)	return 1 means the stack has x, -1 means opposite
 		 */
 		Stack<String> stackTest = new Stack<String>();
 		
 		System.out.println("Check stack's capacity: "+stackTest.empty());
+		System.out.println("Start pushing");
 		stackTest.push("Minecraft");
+		stackTest.push("The sims");
+		stackTest.push("Far cry");
+		stackTest.push("The bubble");
+		System.out.println("Check stack's capacity: "+stackTest.empty());
+		System.out.println("This string was pushed previously: "+stackTest.peek());
+		
+		String game = stackTest.pop(); //it will remove the previous item in stack out
+		
+//		//Study case about the memory and using the stack
+//		for (int i = 0 ; i < 1000000000 ; i++) {
+//			stackTest.push("Papa's");
+//			// it will display an error OutoOfMemoryError
+//			// due to java heap the memory
+//		}
+		
+	}
+	
+	public static void two_Queue() {
+		
 		
 	}
 	
@@ -38,5 +60,16 @@ public class DSOne {
 		System.out.println();
 	}
 	
-
+	public static void detailsInDSOne(String[] details) {
+		System.out.print("Contents : ");
+		for (int i = 0 ; i < details.length ; i++) {
+			System.out.print(details[i] +", ");
+		}
+		}
+	
+	
+	
+//The end of the class		
 }
+	
+
